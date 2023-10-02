2a636fa656e8acbf565171c28759a0873005faa2
a-klarkowska <143771538+a-klarkowska@users.noreply.github.com>
2023-10-02 15:04:09 +0900

    Initial commit

---
+++ R/msScope_nfcore_rnaseq.R
@@ -0,0 +1,35 @@
+#' Function implementing the scope parameter of the \link{mapStats} function
+#' Credit: Charles Plessy
+#' Note: had to remove .checkLibsDataFrame, as it couldn't be imported from CAGEr
+#'
+#' @param libs A data frame containing metadata describing samples in sequence libraries
+#'
+#' @return Returns a list with three elements: `libs` contains a modified
+#' version of the input data frame where columns have been reorganised as needed,
+#' `colums` contains the names of the columns to use for plotting and
+#' provides the order of the stacked bars of the `plotAnnot` function,
+#' `total` indicates the total counts used for normalising the data.
+#'
+#'
+#' @export
+#'
+#'
+msScope_nfcore_rnaseq <- function(libs) {
+  libs$rDNA           <- libs$rdna
+  libs$Unmapped       <-
+    libs$paired_total - libs$paired_aligned_one - libs$paired_aligned_multi
+  libs$Multimapped    <- libs$paired_aligned_multi
+  libs$UniquelyMapped <- libs$paired_aligned_one
+  libs$Discarded      <- libs$realLibrarySizes - libs$extracted
+  list(
+    libs = libs,
+    columns = c(
+      'Discarded',
+      'rDNA',
+      'Unmapped',
+      'Multimapped',
+      'UniquelyMapped'
+    ),
+    total = libs$realLibrarySizes
+  )
+}
---
+++ R/quickGFF.R
@@ -0,0 +1,21 @@
+#' Quickly imports and processes a GFF object as a character vector for use in annotating CAGEexp objects
+#'
+#' @param gff_path path to a GFF file
+#' @param types types of features of interest
+#'
+#' @return GRanges object with selected features
+#' @importFrom rtracklayer import
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' quickGFF('barcelona.gff', types = c('intron', 'exon'))
+#' }
+quickGFF <-
+  function(gff_path,
+           types = c('transcript', 'intron', 'exon')) {
+    gff <- rtracklayer::import(gff_path)
+    gff$type <- as.character(gff$type)
+    gff <- gff[gff$type %in% types]
+    gff$gene_name <- gff$gene_id
+  }
