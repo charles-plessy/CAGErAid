f7654f7d799758f077ba1b3890be3dc69139d8f6
a-klarkowska <143771538+a-klarkowska@users.noreply.github.com>
2023-10-10 13:49:23 +0900

    test fixes

--- tests/testthat/test-quickGFF.R
+++ tests/testthat/test-quickGFF.R
@@ -1,4 +1,4 @@
 test_that("GFF gets imported and processed correctly", {
   gff_path <- '/bucket/LuscombeU/live/CharlesPlessy/CAGE/2022-11-09_Barcelona_Oik/AlignWithRNAseqPipelinePE_BAR/Bar2_p4.gm.gtf'
-  expect_equal(quickGFF(gff_path, 'transcript')[1000], "g930")
+  expect_equal(quickGFF(gff_path, 'transcript')[1000]$gene_id, "g930")
 })
