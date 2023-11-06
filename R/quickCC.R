39256d47398a371dfb1770a6dec42a2e731084cc
a-klarkowska <143771538+a-klarkowska@users.noreply.github.com>
2023-11-06 16:11:23 +0900

    quickCC and quickTC initial commit

---
+++ R/quickCC.R
@@ -0,0 +1,56 @@
+#' Aggregates tag clusters into consensus clusters
+#' @description Quickly aggregates CTSSs into consensus clusters from data from samples with or without splice leader detected with established parameters.
+#' For finding tag clusters, use [quickTC()].
+#'
+#' The method for clustering CTSSs in samples without SL detected is distclu, and with - paraclu. By default, the algorithm uses 4 cores.
+#'
+#' The function annotates found consensus clusters with the use of a GFF file. For easy processing of gff files use [quickGFF()].
+#'
+#' @param ce CAGEexp object
+#' @param sl_found boolean, whether data contains samples with or without SL sequences
+#' @param gff GFF file
+#'
+#' @return CAGEexp object
+#' @importFrom CAGEr normalizeTagCount
+#' @importFrom CAGEr clusterCTSS
+#' @importFrom CAGEr cumulativeCTSSdistribution
+#' @importFrom CAGEr quantilePositions
+#' @importFrom CAGEr aggregateTagClusters
+#' @importFrom CAGEr annotateConsensusClusters
+#'
+#'
+#' @examples
+#' \dontrun{
+#' quickCC(ce, sl_found = TRUE, gff)
+#' }
+#' @export
+quickCC <- function(ce, sl_found, gff) {
+  if (sl_found == FALSE) {
+    ce <- ce |>
+      CAGEr::normalizeTagCount(method = 'simpleTpm') |>
+      CAGEr::clusterCTSS( method = "distclu"
+                          , nrPassThreshold = 1 # Default.  We do not have replicates for all time points
+                          , threshold = 1
+                          , thresholdIsTpm = TRUE)    |>
+      CAGEr::aggregateTagClusters() |>
+      CAGEr::cumulativeCTSSdistribution(clusters = "consensusClusters") |>
+      CAGEr::quantilePositions(clusters = "consensusClusters") |>
+      CAGEr::annotateConsensusClusters(gff)
+  }
+  else {
+    ce <- ce |>
+      # seems to work at the very least
+      # clusterCTSS not with paraclu bc it's copied from Okinawa Rmd, should be with paraclu
+      CAGEr::normalizeTagCount(method = 'simpleTpm') |>
+      CAGEr::clusterCTSS() |>
+      CAGEr::aggregateTagClusters(
+        maxDist = 10L,
+        tpmThreshold = 10,
+        excludeSignalBelowThreshold = FALSE
+      ) |>  # See also the score distribution
+      CAGEr::cumulativeCTSSdistribution(clusters = "consensusClusters") |>
+      CAGEr::quantilePositions(clusters = "consensusClusters") |>
+      CAGEr::annotateConsensusClusters(gff, up = 100, down = 0)
+  }
+  ce
+}
---
+++ R/quickTC.R
@@ -0,0 +1,50 @@
+#' Calculates tag clusters with set parameters
+#' @description Quickly calculates tag clusters from CTSSs in data from samples with or without splice leader detected with established parameters.
+#' For finding consensus clusters, use [quickCC()].
+#'
+#' The method for clustering CTSSs in samples without SL detected is distclu, and with - paraclu. By default, the algorithm uses 4 cores.
+#'
+#'
+#' @param ce CAGEexp object
+#' @param sl_found boolean, whether data contains samples with or without SL sequences
+#'
+#' @return CAGEexp object
+#' @importFrom CAGEr normalizeTagCount
+#' @importFrom CAGEr clusterCTSS
+#' @importFrom CAGEr cumulativeCTSSdistribution
+#' @importFrom CAGEr quantilePositions
+#'
+#'
+#' @examples
+#' \dontrun{
+#' quickTC(ce, sl_found = TRUE)
+#' }
+#' @export
+quickTC <- function(ce, sl_found) {
+  if (sl_found == FALSE) {
+    ce <- ce |>
+      CAGEr::normalizeTagCount(method = 'simpleTpm') |>
+      CAGEr::clusterCTSS(method = "distclu"
+                         , nrPassThreshold = 1
+                         , threshold = 1
+                         , thresholdIsTpm = TRUE)    |>
+      CAGEr::cumulativeCTSSdistribution()                          |>
+      CAGEr::quantilePositions()
+  }
+  else {
+    ce <- ce |>
+      CAGEr::normalizeTagCount(method = 'simpleTpm') |>
+      CAGEr::clusterCTSS(
+        method = "paraclu",
+        nrPassThreshold = 1,
+        threshold = 1,   # it allows low-score CTSS supported in other samples.
+        removeSingletons = TRUE,
+        keepSingletonsAbove = 1,
+        maxLength = 10L, # Keep them sharp
+        useMulticore = TRUE, # Deigo
+        nrCores = 4) |>
+      CAGEr::cumulativeCTSSdistribution() |>
+      CAGEr::quantilePositions()
+  }
+  ce
+}
